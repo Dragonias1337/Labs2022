fa8bedf95d4e19851990c3ae59c5b985deb77b28
Максим Мисюра <m_misyura@list.ru>
2022-10-23 19:55:13 +0300

    Task 2

---
+++ Lab1/Lab4/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Car
+    {
+        private string name;
+        private int productionYear;
+        private int maxSpeed;
+
+        public string Name { get => name; set => name = value; }
+        public int ProductionYear { get => productionYear; set => productionYear = value; }
+        public int MaxSpeed { get => maxSpeed; set => maxSpeed = value; }
+
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {name}, Prod. Year: {productionYear}, Max Speed: {maxSpeed}");        
+        }
+    }
+
+    class CarComparerByName : IComparer<Car>
+    {
+        int IComparer<Car>.Compare(Car? x, Car? y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+    class CarComparerByProdYear : IComparer<Car>
+    {
+        int IComparer<Car>.Compare(Car? x, Car? y)
+        {
+            return x.ProductionYear - y.ProductionYear;
+        }
+    }
+    class CarComparerByMaxSpeed : IComparer<Car>
+    {
+        int IComparer<Car>.Compare(Car? x, Car? y)
+        {
+            return x.MaxSpeed - y.MaxSpeed;
+        }
+    }
+
+}
--- Lab1/Lab4/Program.cs
+++ Lab1/Lab4/Program.cs
@@ -6,13 +6,50 @@
         {
             //ввод с консоли
 
-            MyMatrix a = new MyMatrix(3, 3, -1, 1);
-            MyMatrix b = new MyMatrix(3, 3, -1, 1);
-            MyMatrix c = a + b;
+            //MyMatrix a = new MyMatrix(3, 3, -1, 1);
+            //MyMatrix b = new MyMatrix(3, 3, -1, 1);
+            //MyMatrix c = a + b;
 
-            a.Show();
-            b.Show();
-            c.Show();
+            //a.Show();
+            //b.Show();
+            //c.Show();
+
+            Car[] cars = new Car[] {
+            new Car()
+            {
+                Name = "1488",
+                ProductionYear = 1994,
+                MaxSpeed = 100
+            },
+            new Car()
+            {
+                Name = "123",
+                ProductionYear = 1999,
+                MaxSpeed = 50
+            },
+            new Car()
+            {
+                Name = "22",
+                ProductionYear = 2002,
+                MaxSpeed = 60
+            }
+            };
+
+            foreach (var o in cars) o.Show();
+            Console.WriteLine();
+
+            Array.Sort(cars, new CarComparerByName());
+            foreach (var o in cars) o.Show();
+            Console.WriteLine();
+
+            Array.Sort(cars, new CarComparerByProdYear());
+            foreach (var o in cars) o.Show();
+            Console.WriteLine();
+
+            Array.Sort(cars, new CarComparerByMaxSpeed());
+            foreach (var o in cars) o.Show();
+            Console.WriteLine();
+
 
         }
     }
