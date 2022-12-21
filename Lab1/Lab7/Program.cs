1f70c85346d12095bbe87d46017bd54aab21fbf8
Максим Мисюра <m_misyura@list.ru>
2022-12-21 21:31:07 +0300

    Lab7

---
+++ Lab1/Lab7/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lab7Library;
+using System.Reflection;
+using System.Xml;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cow c1 = new Lab7Library.Cow()
+            {Name = "Ivan",
+            Country = "Russia"};
+
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlDeclaration xmlDec = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", String.Empty);
+            Type idType = c1.GetType();
+            xmlDoc.PrependChild(xmlDec);
+            XmlElement elemRoot = xmlDoc.CreateElement(idType.ToString());
+            xmlDoc.AppendChild(elemRoot);
+            XmlElement elem = null;            
+            foreach (PropertyInfo pInfo in idType.GetProperties())
+            {
+                object o = pInfo.GetValue(c1, null);
+                elem = xmlDoc.CreateElement(pInfo.Name);
+                if(o!=null)
+                elem.InnerText = o.ToString();
+                elemRoot.AppendChild(elem);
+            }
+            xmlDoc.Save("123.xml");
+
+        }
+    }
+}
---
+++ Lab1/Lab7Library/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Lab7Library
+{
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavoriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    public abstract class Animal
+    {
+        private string country, name;
+        private bool hideFromOtherAnimals;
+        private eClassificationAnimal whatAnimal;
+
+        public string Country { get => country; set => country = value; }
+        public string Name { get => name; set => name = value; }
+        public bool HideFromOtherAnimals { get => hideFromOtherAnimals; set => hideFromOtherAnimals = value; }
+        public eClassificationAnimal WhatAnimal { get => whatAnimal; set => whatAnimal = value; }
+
+        public Animal() { }
+        public void Decontruct() { }
+        public eClassificationAnimal GetClassificationAnimal() { return WhatAnimal; }
+        public abstract eFavoriteFood GetFavoriteFood();
+        public abstract void SayHello();
+
+    }
+
+    [AttributeForBykov]
+    public class Cow : Animal
+    {
+        public Cow() 
+        {
+            WhatAnimal = eClassificationAnimal.Herbivores;
+            HideFromOtherAnimals = false;
+        }
+
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            return eFavoriteFood.Plants;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Bruh");
+        }
+    }
+
+    class AttributeForBykov : Attribute
+    {
+        public void Comment()
+        {
+            Console.WriteLine("Vingardium fartiosa!");
+        }
+    }
+
+}
