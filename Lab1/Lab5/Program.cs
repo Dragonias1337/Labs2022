bd44bf737c4d05dcc3fbccaaa09ca27ed26e9fd6
Максим Мисюра <m_misyura@list.ru>
2023-01-23 19:51:12 +0300

    5 lab is done!

--- Lab1/Lab5/Program.cs
+++ Lab1/Lab5/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            MyMatrix a = new MyMatrix(5, 4, -1, 1);
+            //Все для 1 задания
+            //MyMatrix a = new MyMatrix(5, 4, -1, 1);
             //a.Show();
             //Console.WriteLine();
 
@@ -15,15 +16,33 @@
             //a.Show();
             //Console.WriteLine();
 
-            MyList<String> b = new MyList<String>();
-            b.Append("123");
-            b.Append("456");
-            b.Append("789");
-            Console.WriteLine(b.Count());
 
-            foreach(var o in b) Console.WriteLine(o.ToString());
+            //Все для 2 задания
+            //MyList<String> b = new MyList<String>();
+            //b.Append("123");
+            //b.Append("456");
+            //b.Append("789");
+            //Console.WriteLine(b.Count());
 
-            Console.WriteLine(b[2]);
+            //foreach(var o in b) Console.WriteLine(o.ToString());
+
+            //Console.WriteLine(b[2]);
+
+            //Все для 3 задания
+            MyDictionary<int, string> mydic = new MyDictionary<int, string>(10);
+            mydic.Add(1001, "Ivan");
+            mydic.Add(1100, "Petr");
+            mydic[1010] = "Egor";
+
+            Console.WriteLine($"Всего в словаре {mydic.Lenght} элементов:\n");
+            foreach (var item in mydic)
+            {
+                Console.WriteLine($"mydic[{item.Key}] = {item.Value}");
+            }
+
+
+
+
 
         }
     }
