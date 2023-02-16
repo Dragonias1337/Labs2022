bc4c988c4d79fbcf50b6077e55b6ab24544b8945
Максим Мисюра <m_misyura@list.ru>
2023-02-16 17:07:49 +0300

    Lab 3 is done

---
+++ Lab1/Lab3/Currency.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Currency
+    {
+        private double value;
+
+        public double Value { get => value; set => this.value = value; }
+
+    }
+
+    class CurrencyUSD : Currency
+    {
+        private static double changeMultiplierRUB = 0.013476;
+        private static double changeMultiplierEUR = 1.07;
+
+        public static implicit operator CurrencyUSD(CurrencyEUR x)
+        {
+            return new CurrencyUSD { Value = x.Value * changeMultiplierEUR };
+        }
+        public static implicit operator CurrencyUSD(CurrencyRUB x)
+        {
+            return new CurrencyUSD { Value = x.Value * changeMultiplierRUB };
+        }
+    }
+
+    class CurrencyEUR : Currency
+    {
+        private static double changeMultiplierRUB = 0.012568;
+        private static double changeMultiplierUSD = 0.93406;
+
+        public static implicit operator CurrencyEUR(CurrencyUSD x)
+        {
+            return new CurrencyEUR { Value = x.Value * changeMultiplierUSD };
+        }
+        public static implicit operator CurrencyEUR(CurrencyRUB x)
+        {
+            return new CurrencyEUR { Value = x.Value * changeMultiplierRUB };
+        }
+    }
+
+    class CurrencyRUB : Currency
+    {
+        private static double changeMultiplierEUR = 79.57;
+        private static double changeMultiplierUSD = 74.21;
+
+        public static implicit operator CurrencyRUB(CurrencyUSD x)
+        {
+            return new CurrencyRUB { Value = x.Value * changeMultiplierUSD };
+        }
+        public static implicit operator CurrencyRUB(CurrencyEUR x)
+        {
+            return new CurrencyRUB { Value = x.Value * changeMultiplierEUR };
+        }
+    }
+
+
+
+
+}
--- Lab1/Lab3/Program.cs
+++ Lab1/Lab3/Program.cs
@@ -4,11 +4,19 @@
     {
         static void Main(string[] args)
         {
-            CarsCatalog c1 = new CarsCatalog();
-            for (int i = 0; i < c1.Soses.Count; i++)
-            {
-                Console.WriteLine(c1[i]);
-            }
+            //CarsCatalog c1 = new CarsCatalog();
+            //for (int i = 0; i < c1.Soses.Count; i++)
+            //{
+            //    Console.WriteLine(c1[i]);
+            //}
+
+            CurrencyEUR wallet = new CurrencyEUR { Value = 1000 };
+            Console.WriteLine($"В кошельке {wallet.Value} евро");
+            CurrencyRUB wallet2 = wallet;
+            Console.WriteLine($"Неявное преобразование: в кошельке {wallet2.Value} руб");
+            CurrencyUSD wallet3 = (CurrencyUSD)wallet;
+            Console.WriteLine($"Явное преобразование: в кошельке {wallet3.Value} $");
+
         }
     }
 }
